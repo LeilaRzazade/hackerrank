fd8eeab836e7b4559e974740b7a39b473693bbdf
Leila <42345795+LeilaRzazade@users.noreply.github.com>
2019-07-07 13:22:40 +0400

    Add files via upload

---
+++ guess_number.py
@@ -0,0 +1,17 @@
+import random
+random_num = random.randint(1,100)
+user_input = int(input("Guess the number: "))
+if (user_input == random_num):
+    print("Congratulations! Guessed number is: ", user_input)
+while(user_input != random_num):
+    if(user_input == random_num):
+        print("Congratulations! Guessed number is right.")
+        break
+    else:
+        if(user_input > random_num):
+            print("Your number is not right. It's too high.")
+            user_input = int(input(("Enter a new number: ")))
+        else:
+            print("Your number is not right. It's too low.")
+            user_input = int(input(("Enter a new number: ")))
+print("Congratulations! Guessed number is: ", user_input)
